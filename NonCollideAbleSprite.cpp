702d5549c656ac0fd2f0c7a80a3aa7368489e164
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2021-12-09 20:27:14 +0100

    Update NonCollideAbleSprite.cpp

--- NonCollideAbleSprite.cpp
+++ NonCollideAbleSprite.cpp
@@ -1,3 +1,4 @@
+//Code written by Christian Neij
 #include "NonCollideAbleSprite.h"
 namespace minMotor {
 	NonCollideAbleSprite::NonCollideAbleSprite(int x, int y, int w, int h):Sprite(x,y,w,h,false) {
